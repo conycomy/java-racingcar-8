04efc98dc0b2e4127f63dd72b9979a5d986682b9
conycomy <imgyeong4936@gmail.com>
2025-10-25 15:34:38 +0900

    refactor: RandomNumberGeneratorTest를 util 패키지로 이동

--- src/test/java/racingcar/util/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/util/RandomNumberGeneratorTest.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.util;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.util.RandomNumberGenerator;
 
 public class RandomNumberGeneratorTest {
 

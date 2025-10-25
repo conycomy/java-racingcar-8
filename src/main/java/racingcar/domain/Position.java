2c44aa8f2a386e476b9c994b7eb73e0abd99ccc0
conycomy <imgyeong4936@gmail.com>
2025-10-25 17:02:12 +0900

    feat: 위치 관리를 위한 Position 클래스 구현

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,4 +1,26 @@
 package racingcar.domain;
 
 public class Position {
+
+    private final int position;
+
+    public Position(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("위치는 0 이상이어야 합니다.");
+        }
+        this.position = position;
+    }
+
+    public Position advance(boolean isMoveable) {
+        if (isMoveable) {
+            return new Position(position + 1);
+        }
+        return this;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
 }
+

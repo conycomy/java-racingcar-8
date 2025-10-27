3634a2cd97ae41186a9d0d24a625a34db151250a
conycomy <imgyeong4936@gmail.com>
2025-10-27 22:34:21 +0900

    feat: Application 메인 진입점 구현 및 객체 조립 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import racingcar.domain.rule.RacingRule;
+import racingcar.util.NameValidator;
+import racingcar.util.NumberGenerator;
+import racingcar.util.RandomNumberGenerator;
+import racingcar.io.InputView;
+import racingcar.io.ResultView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NameValidator nameValidator = new NameValidator();
+
+        NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        RacingRule racingRule = new RacingRule(randomNumberGenerator);
+
+        InputView inputView = new InputView(nameValidator);
+
+        ResultView resultView = new ResultView();
+
+        RacingGame game = new RacingGame(inputView, resultView, racingRule);
+
+        game.play();
     }
 }

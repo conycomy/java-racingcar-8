05d8f7fac3388314a78eb6a359181bc388a895a3
conycomy <imgyeong4936@gmail.com>
2025-10-27 14:46:33 +0900

    feat: RaceParticipant 인터페이스 정의 및 Car 클래스 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import racingcar.domain.rule.RacingRule;
+
+public class Car implements RaceParticipant {
+    private final String name;
+    private Position position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = new Position(0);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Position getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public void move(RacingRule rule) {
+        boolean isMoveable = rule.isMoveable();
+        this.position = this.position.advance(isMoveable);
+
+    }
+}
---
+++ src/main/java/racingcar/domain/RaceParticipant.java
@@ -0,0 +1,10 @@
+package racingcar.domain;
+
+import racingcar.domain.rule.RacingRule;
+
+public interface RaceParticipant {
+    String getName();
+    Position getPosition();
+    void move(RacingRule rule);
+
+}

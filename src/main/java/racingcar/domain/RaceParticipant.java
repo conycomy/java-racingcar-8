9a5b267766ecaf35b17d8d95bec2a1b123f6df29
conycomy <imgyeong4936@gmail.com>
2025-10-27 22:40:43 +0900

    style: RaceParticipant 인터페이스의 포맷팅(빈 줄 수정)

--- src/main/java/racingcar/domain/RaceParticipant.java
+++ src/main/java/racingcar/domain/RaceParticipant.java
@@ -3,8 +3,12 @@ package racingcar.domain;
 import racingcar.domain.rule.RacingRule;
 
 public interface RaceParticipant {
+
     String getName();
+
     Position getPosition();
+
     void move(RacingRule rule);
+
 
 }

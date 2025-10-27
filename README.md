64dacb5b5d6f650e7c5626b408e5e4c943d80c23
conycomy <imgyeong4936@gmail.com>
2025-10-27 19:44:27 +0900

    feat: NameValidator 구현 및 이름 유효성 검증 로직 추가

--- README.md
+++ README.md
@@ -11,10 +11,10 @@
   - RaceParticipant 참가자 공통 규격 정의 (인터페이스) [ O ]
   - Car : 이름 + 위치, move() 구현 [ O ]
   - RaceResult : 최대 거리, 우승자 계산 [ O ]
-  - RaceParticipants : 전체 이동, 결과 생성 [ ]
+  - RaceParticipants : 전체 이동, 결과 생성 [ O ]
 
  검증 & 입출표 
-  - 쉼표 구분, 5자 이하 검사 [ ]
+  - 쉼표 구분, 5자 이하 검사 [ O ]
   - 이름, 시도 횟수 입력 [ ]
   - 실행 결과, 우승자 출력 [ ]
 
---
+++ src/main/java/racingcar/util/NameValidator.java
@@ -0,0 +1,48 @@
+package racingcar.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NameValidator {
+
+    public List<String> splitByComma(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    //2. 이름 길이를 검증하는 책임
+    public void validateNameLength(List<String> names) {
+        boolean isOverLength = names.stream()
+                .anyMatch(name -> name.length() > 5);
+
+        if (isOverLength) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+
+    }
+
+    // 3. 이름이 비어있거나 중복되는지 검증되는 책임
+    public void validateEmptyOrDuplicate(List<String> names) {
+        if (names.isEmpty() || names.contains("")) {
+            throw new IllegalArgumentException("이름은 쉼표 외에 공백일 수 없으며, 최소 1개 이상을 입력 해야 합니다.");
+        }
+        long distinctCount = names.stream()
+                .distinct()
+                .count();
+        if(distinctCount != names.size()) {
+            throw new IllegalArgumentException("이름은 중복될 수 없습니다. 다른 닉네임을 설정해주세요");
+        }
+    }
+
+    public List<String> validateAndGetNames(String input) {
+        List<String> names = splitByComma(input);
+        validateNameLength(names);
+        validateEmptyOrDuplicate(names);
+        return names;
+
+    }
+
+
+}

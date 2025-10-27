d4dd779a9feb54daf16c89e4d098ef01cfd43571
conycomy <imgyeong4936@gmail.com>
2025-10-27 22:35:07 +0900

    refactor: NumberGenerator 인터페이스 정의 및 RacingRule의 의존성 분리

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,7 +1,7 @@
 package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator {
    public int generate() {
         final int START_RANGE = 0;
         final int END_RANGE = 9;

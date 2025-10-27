88bb3601221fcc72dbfc1cf419f74cff0a40b62c
conycomy <imgyeong4936@gmail.com>
2025-10-27 14:48:59 +0900

    feat: RaceResult 구현 및 우승자 판별 로직 추가

--- README.md
+++ README.md
@@ -8,8 +8,9 @@
   - 0~9 난수 생성 [ O ]
   - 4 이상 전진 여부 판단 [ O ]
   - 위치 관리 [ O ]
-  - Car : 이름 + 위치, move() 구현 [ ]
-  - RaceResult : 최대 거리, 우승자 계산 [ ]
+  - RaceParticipant 참가자 공통 규격 정의 (인터페이스) [ O ]
+  - Car : 이름 + 위치, move() 구현 [ O ]
+  - RaceResult : 최대 거리, 우승자 계산 [ O ]
   - RacingCars : 전체 이동, 결과 생성 [ ]
 
  검증 & 입출표 
--- src/main/java/racingcar/domain/RaceResult.java
+++ src/main/java/racingcar/domain/RaceResult.java
@@ -1,4 +1,32 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RaceResult {
+
+    private final int maxDistance;
+    private final List<RaceParticipant> winners;
+
+
+    public RaceResult(List<RaceParticipant> participants) {
+        this.maxDistance = caculateMaxDistance(participants);
+        this.winners = findWinners(participants, this.maxDistance);
+    }
+
+    private List<RaceParticipant> findWinners(List<RaceParticipant> participants, int maxDistance) {
+        return participants.stream()
+                .filter(p -> p.getPosition().getPosition() == maxDistance)
+                .collect(Collectors.toList());
+
+    }
+
+    private int caculateMaxDistance(List<RaceParticipant> participants) {
+        return participants.stream()
+                .mapToInt(p -> p.getPosition().getPosition())
+                .max()
+                .orElse(0);
+    }
 }
+

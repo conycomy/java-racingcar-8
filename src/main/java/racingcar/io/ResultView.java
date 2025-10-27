a3d66658ceda22435177f0b6d7baddc016e666e8
conycomy <imgyeong4936@gmail.com>
2025-10-27 20:06:16 +0900

    feat: ResultView 구현 및  최종 결과 출력 로직 추가

--- src/main/java/racingcar/io/ResultView.java
+++ src/main/java/racingcar/io/ResultView.java
@@ -1,4 +1,42 @@
 package racingcar.io;
 
+import racingcar.domain.RaceParticipant;
+import racingcar.domain.RaceResult;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultView {
+
+    public void printStartMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void printRoundResult(List<RaceParticipant> participants) {
+        for (RaceParticipant participant : participants) {
+            System.out.print(participant.getName() + " : ");
+
+            int distance = participant.getPosition().getPosition();
+            printDash(distance);
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    public void printWinners(RaceResult result) {
+        List<RaceParticipant> winners = result.getName();
+
+        String winnerNames = winners.stream()
+                .map(RaceParticipant::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
+
+    // private 도우미 메서드: 거리에 따라 '-'를 출력
+    private void printDash(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print("-");
+        }
+    }
 }
